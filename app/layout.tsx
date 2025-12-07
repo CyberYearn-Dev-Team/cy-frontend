f07d1bef894d16dc4af76f992ab0deced7c78585
Bby Lion <codelab042@gmail.com>
2025-12-07 10:02:32 +0100

    user management api all added

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* Client-side inactivity monitor */}
+        {/* Client side inactivity monitor for (loging user out) */}
         <InactivityHandler />
 
         {children}
