06e45454c1317ad0b9df97018133075b252ed9cb
Igbokwe Favour <igbokwefc1@gmail.com>
2026-05-29 21:07:40 +0100

    open-next

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,8 +1,8 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import r2IncrementalCache from "@opennextjs/cloudflare/dist/api/incremental-cache/r2.js";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
 export default defineCloudflareConfig({
   incrementalCache: r2IncrementalCache({
-    populateCache: false,  
+    populateCache: false,
   }),
 });
